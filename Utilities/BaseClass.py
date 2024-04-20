aa30a5bbde8c67e5ec947bdf96b29b3baaf724f1
bhutaniaayush <aayushbhutani999@gmail.com>
2024-04-20 23:07:08 +0530

    Update BaseClass.py

--- Utilities/BaseClass.py
+++ Utilities/BaseClass.py
@@ -8,15 +8,12 @@ class BaseClass:
         datefmt="%Y-%m-%d %H:%M:%S"
     )
 
-    # Define a file handler
     file_handler = logging.FileHandler('test.log')
     file_handler.setLevel(logging.INFO)
 
-    # Define the log format for the file handler
     file_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
     file_handler.setFormatter(file_formatter)
 
-    # Add the file handler to the logger
     logger.addHandler(file_handler)
 
     @classmethod
